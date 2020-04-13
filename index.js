14b0b582812b7fc916930a0fb30f7c6c47abb693
Chris Xu <gzchrisxjh@gmail.com>
2020-04-13 18:52:52 -0400

    Use port from env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const gomoku = require('./games/gomoku');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
